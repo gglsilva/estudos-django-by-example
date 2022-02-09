2de7b2071e5c17f355df2c3bae0f08f6954d08fa
gglsilva <gabrielchamex@gmail.com>
2022-02-09 14:53:25 -0300

    Criando decoradores personalizados para as views

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 from django.contrib import messages
+from bookmarks.commom.decorators import ajax_required
 from .models import Image
 from .forms import ImageCreateForm
 
@@ -47,6 +48,8 @@ def image_detail(request, id, slug):
                   {'section': 'images',
                    'image': image})
 
+
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
