04eb596f823d79684f5c8aab35ef26cf52f85641
gglsilva <gabrielchamex@gmail.com>
2022-02-22 11:53:01 -0300

    Criando sistema de Segui

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,5 +1,8 @@
+from pyexpat import model
+from venv import create
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 class Profile(models.Model):
@@ -9,3 +12,25 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
+
+
+class Contact(models.Model):
+    user_from = models.ForeignKey('auth.User', related_name='rel_from_set',on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User', related_name='rel_to_set',on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True,db_index=True)
+
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+
+
+# Adiciona o campo seguindo para o usuaio dinamicamente
+user_model = get_user_model()
+user_model.add_to_class('following',
+                        models.ManyToManyField('self',
+                        through=Contact,
+                        related_name='followers',
+                        symmetrical=False))
